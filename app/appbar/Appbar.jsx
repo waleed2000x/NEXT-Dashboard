ec5f9a6ee1829612d8eda76b8448827c3d7d7e1e
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-26 13:51:26 +0500

    buttons and padding | Appbar

--- app/appbar/Appbar.jsx
+++ app/appbar/Appbar.jsx
@@ -1,11 +1,13 @@
 "use client";
 import { Button, Input } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
+import SearchIcon from "@mui/icons-material/Search";
+import MonitorHeartOutlinedIcon from "@mui/icons-material/MonitorHeartOutlined";
 export default function Appbar() {
   return (
     <div className="appbar-parent">
       <div className="appbar-center">
         <div className="logo">
+          <MonitorHeartOutlinedIcon />
           <h1>DataPulse</h1>
         </div>
         <div className="nav-items">
