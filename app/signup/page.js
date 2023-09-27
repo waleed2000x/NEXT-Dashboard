6bfcdcf52f6f252125bef0a32ee790db0538f02e
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-27 22:09:33 +0500

    Sign Up

--- app/signup/page.js
+++ app/signup/page.js
@@ -6,16 +6,50 @@ export default function page() {
     <div className="parent-signup">
       <div className="signup-card">
         <div className="signup-form">
-          <h1>Signup</h1>
+          <h1>
+            <b>Signup</b>
+          </h1>
           <div className="signup-inputs">
             <div className="input-row">
               <div className="input">
                 <FormLabel>First Name</FormLabel>
-                <Input variant="filled" />
+                <Input
+                  variant="outline"
+                  borderColor="white"
+                  focusBorderColor="white"
+                  type="text"
+                  backgroundColor="transparent"
+                />
               </div>
               <div className="input">
                 <FormLabel>Last Name</FormLabel>
-                <Input variant="filled" />
+                <Input
+                  variant="outline"
+                  borderColor="white"
+                  focusBorderColor="white"
+                  type="password"
+                />
+              </div>
+            </div>
+            <div className="input-row">
+              <div className="input">
+                <FormLabel>Email</FormLabel>
+                <Input
+                  variant="outline"
+                  borderColor="white"
+                  focusBorderColor="white"
+                  type="email"
+                  backgroundColor="transparent"
+                />
+              </div>
+              <div className="input">
+                <FormLabel>Contact</FormLabel>
+                <Input
+                  variant="outline"
+                  borderColor="white"
+                  focusBorderColor="white"
+                  type="text"
+                />
               </div>
             </div>
           </div>
