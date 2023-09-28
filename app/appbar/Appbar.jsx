527e438047ae24c7cd26d683e213e9d16868a4bb
waleed2000× <waleedahmed2000x@gmail.com>
2023-09-28 22:57:23 +0500

    Alternative

--- app/appbar/Appbar.jsx
+++ app/appbar/Appbar.jsx
@@ -1,17 +1,12 @@
-import {
-  Button,
-  Input,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
-} from "@chakra-ui/react";
-import SearchIcon from "@mui/icons-material/Search";
+"use client";
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import PersonIcon from "@mui/icons-material/Person";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import MonitorHeartOutlinedIcon from "@mui/icons-material/MonitorHeartOutlined";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function Appbar() {
+  const route = usePathname();
   return (
     <div className="appbar-parent">
       <div className="appbar-center">
@@ -23,54 +18,68 @@ export default function Appbar() {
         </div>
         <div className="nav-items">
           <div className="nav-search">
-            <Menu>
-              <MenuButton
-                _hover={{ bg: "#6bb3e3" }}
-                as={Button}
-                variant="ghost"
-                size="lg"
-                rightIcon={<ArrowDropDownIcon />}
-                className="menu-icon-appbar"
-              >
-                <PersonIcon />
-              </MenuButton>
-              <MenuList
-                style={{
-                  backgroundColor: "#3498DB",
-                  padding: "10px",
-                  width: "300px",
-                }}
-              >
-                <MenuItem _hover={{ bg: "#6bb3e3" }} backgroundColor="#3498DB">
-                  <b>Account</b>
-                </MenuItem>
-                <MenuItem _hover={{ bg: "#6bb3e3" }} backgroundColor="#248bd1">
-                  <b>Profile</b>
-                </MenuItem>
-                <MenuItem _hover={{ bg: "#6bb3e3" }} backgroundColor="#3498DB">
-                  <b>Settings</b>
-                </MenuItem>
-                <Link href="/login">
-                  <MenuItem
-                    _hover={{ bg: "#6bb3e3" }}
-                    backgroundColor="#248bd1"
-                  >
-                    <b>Login</b>
-                  </MenuItem>
-                </Link>
-                <Link href="/signup">
+            {route === "/login" && route === "/signup" ? null : (
+              <Menu>
+                <MenuButton
+                  _hover={{ bg: "#6bb3e3" }}
+                  as={Button}
+                  variant="ghost"
+                  size="lg"
+                  rightIcon={<ArrowDropDownIcon />}
+                  className="menu-icon-appbar"
+                >
+                  <PersonIcon />
+                </MenuButton>
+                <MenuList
+                  style={{
+                    backgroundColor: "#3498DB",
+                    padding: "10px",
+                    width: "300px",
+                  }}
+                >
                   <MenuItem
                     _hover={{ bg: "#6bb3e3" }}
                     backgroundColor="#3498DB"
                   >
-                    <b>Signup</b>
+                    <b>Account</b>
                   </MenuItem>
-                </Link>
-                <MenuItem _hover={{ bg: "#6bb3e3" }} backgroundColor="#248bd1">
-                  <b>Logout</b>
-                </MenuItem>
-              </MenuList>
-            </Menu>
+                  <MenuItem
+                    _hover={{ bg: "#6bb3e3" }}
+                    backgroundColor="#248bd1"
+                  >
+                    <b>Profile</b>
+                  </MenuItem>
+                  <MenuItem
+                    _hover={{ bg: "#6bb3e3" }}
+                    backgroundColor="#3498DB"
+                  >
+                    <b>Settings</b>
+                  </MenuItem>
+                  <Link href="/login">
+                    <MenuItem
+                      _hover={{ bg: "#6bb3e3" }}
+                      backgroundColor="#248bd1"
+                    >
+                      <b>Login</b>
+                    </MenuItem>
+                  </Link>
+                  <Link href="/signup">
+                    <MenuItem
+                      _hover={{ bg: "#6bb3e3" }}
+                      backgroundColor="#3498DB"
+                    >
+                      <b>Signup</b>
+                    </MenuItem>
+                  </Link>
+                  <MenuItem
+                    _hover={{ bg: "#6bb3e3" }}
+                    backgroundColor="#248bd1"
+                  >
+                    <b>Logout</b>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            )}
           </div>
         </div>
       </div>
--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ChakraProvider>{children}</ChakraProvider>
       </body>
     </html>
